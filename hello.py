0dc687c0613e3152223ad71268924414157d53e7
otaku904 <domu904@gmail.com>
2020-02-17 18:04:24 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,21 @@
 
 #%%
+import time
+import sys
 msg = "Hello World"
 print(msg)
 
 #%%
 msg = "Hello again"
 print(msg)
+
+
+# %%
+for i in range(8):
+    print(i)
+    time.sleep(0.5)
+
+
+# %%
+for i in range(50):
+    print(i)
