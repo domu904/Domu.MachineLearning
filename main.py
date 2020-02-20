00ecf5d5279381322604c1d0157ff99ce165d469
otaku904 <domu904@gmail.com>
2020-02-19 22:59:13 -0600

    included standard plot py

--- main.py
+++ main.py
@@ -5,20 +5,9 @@ import sys
 msg = "Hello World"
 print(msg)
 
-#%%
-msg = "Hello again"
-print(msg)
-
 
 # %%
 for i in range(8):
     print(i)
     time.sleep(0.5)
 
-
-# %%
-for i in range(50):
-    print(i)
-
-
-# %%
